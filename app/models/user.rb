19d6405e64ee83ec85cc265dcb785748a78cf168
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-10-30 20:32:50 +0200

    Fix reset_token error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-    attr_reader :password 
+    attr_reader :password
+    
+    after_initialize :ensure_session_token
+
     validates :user_name, :password_digest, :session_token, presence: true 
     validates :user_name, uniqueness: true
-    validates :password, length: {minimum: 6}
-    before_validation :ensure_session_token
+    validates :password, length: {minimum: 6, allow_nil: true}
 
     has_many :subs, 
         foreign_key: :moderator_id,
@@ -12,17 +14,21 @@ class User < ApplicationRecord
     has_many :posts, 
         foreign_key: :author_id,
         class_name: 'Post'
+    
+    has_many :comments,
+        foreign_key: :author_id,
+        class_name: 'Comment'
 
     def self.find_by_credentials(user_name, password)
         user = User.find_by(user_name: user_name)
-        unless user.nil?
-            return (user.is_password?(password) ? user : nil)
-        end
-        nil 
+        user.try(:is_password?, password) ? user : nil 
     end
 
     def self.generate_session_token
-        SecureRandom.urlsafe_base64(16) 
+        begin 
+        token = SecureRandom.urlsafe_base64(16)
+        end while User.exists?(session_token: token)
+        token  
     end
 
     def ensure_session_token
@@ -35,11 +41,12 @@ class User < ApplicationRecord
     end
 
     def is_password?(password)
-        # BCrypt::Password.create(password) == BCrypt::Password.new(self.password_digest)
         BCrypt::Password.new(self.password_digest).is_password?(password)        
     end
 
     def reset_session_token!
         self.session_token = User.generate_session_token
+        self.save!
+        self.session_token
     end
 end
