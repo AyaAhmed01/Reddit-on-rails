53b93334234490dde968d69b95e236a798b451f4
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-10-30 20:36:41 +0200

    Fix auth_token

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
     def auth_token_helper 
-        "<input type=\"hidden\" method=\"authenticity_token\" name=\"%<=form_authenticity_token%>\">".html_safe
+        "<input type=\"hidden\" name=\"authenticity_token\" value=\"<%=#{form_authenticity_token}%>\">".html_safe
+    end
+
+    def comment_link_helper(comment)
+        content = comment.content
+        if content.length > 20
+            text = "#{content[0..20]}..."
+        else
+            text = "#{content}"
+        end
+        text.html_safe
     end
 end
