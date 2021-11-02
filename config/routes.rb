f2848c242233dd2dc81fa8cca943d32ced1b24d2
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-02 16:17:56 +0200

    Add subscriptions member routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:create, :new]
   resource :session, only: [:new, :create, :destroy]
-  resources :subs 
+  resources :subs do 
+    member do 
+      delete :unsubscribe 
+      post :subscribe 
+    end
+  end
   resources :posts, except: :index do 
     resources :comments, only: :new 
     post 'upvote', on: :member
