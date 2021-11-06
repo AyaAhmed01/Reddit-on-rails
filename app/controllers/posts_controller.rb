cf388f194021254e338e272f29b08000b1ec6307
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-06 17:36:43 +0200

    Modify #require_author!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController 
-    before_action :require_signed_in!, only: %i(new create upvote downvote)
+    before_action :require_signed_in!, except: :show
     before_action :require_author!, only: %i(edit update destroy)
 
     def create
@@ -32,7 +32,7 @@ class PostsController < ApplicationController
     end
 
     def destroy
-        post = Post.find(params[:id])
+        post = Post.friendly.find(params[:id])
         post_sub = post.sub 
         post.destroy
         redirect_to sub_url(post_sub)
@@ -56,13 +56,13 @@ class PostsController < ApplicationController
         params.require(:post).permit(:title, :content, :url, sub_ids:[])
     end
 
-    def require_author
-        post = Post.find(params[:id])
-        redirect_to post_url(post) unless post.author == current_user
+    def require_author!
+        post = Post.friendly.find(params[:id])
+        render json: "Forbidden", status: :forbidden unless post.author == current_user
     end
 
     def vote(vote_value)
-        @post = Post.find(params[:id])
+        @post = Post.friendly.find(params[:id])
         @user_vote = @post.votes.find_or_initialize_by(user_id: current_user.id)
         @user_vote.update(value: vote_value)
         redirect_to post_url(@post) 
