2ee1b091f9ffca6f0fec2d594e270c6529b36e21
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-06 07:56:34 +0200

    Modify #create to send activation email and add #activate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save
-            log_in_user!(@user)  
-            redirect_to subs_url
+            email = UserMailer.activation_email(@user)
+            email.deliver_now    # send the activation email to user email 
+            flash[:notice] = "Successfully created your account! check your inbox for an activation email" 
+            redirect_to new_session_url 
         else
             flash.now[:errors] = @user.errors.full_messages
             render :new 
@@ -24,8 +26,16 @@ class UsersController < ApplicationController
         @total_vote_score = voted_posts_score + voted_comments_score
     end
 
+    def activate 
+        @user = User.find_by(activation_token: params[:activation_token])
+        @user.try(:activate!)
+        flash[:notice] = "Successfully activated your account!"
+        log_in_user!(@user) 
+        redirect_to root_url  
+    end
+
     private
     def user_params 
-        params.require(:user).permit(:user_name, :password)
+        params.require(:user).permit(:user_name, :password, :email)
     end
 end
