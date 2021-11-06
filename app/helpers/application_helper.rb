363b5b574bfd29d2dae16e758014392dab7e5df1
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-06 20:14:24 +0200

    Delete comment_link_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,9 @@
 module ApplicationHelper
-    def auth_token_helper 
-        "<input type=\"hidden\" name=\"authenticity_token\" value=\"<%=#{form_authenticity_token}%>\">".html_safe
-    end
-
-    def comment_link_helper(comment)
-        content = comment.content
-        if content.length > 20
-            text = "#{content[0..20]}..."
-        else
-            text = "#{content}"
-        end
-        text.html_safe
+    def auth_token_helper
+        "<input
+        type='hidden'
+        name='authenticity_token'
+        value='#{form_authenticity_token}'
+        />".html_safe 
     end
 end
