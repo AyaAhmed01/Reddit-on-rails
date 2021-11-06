a4fedcbbe4094920a8a063f32e4ca07142cb399e
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-06 17:04:22 +0200

    Add activate route for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
-  resources :users, only: [:create, :new, :show]
+  resources :users, only: [:create, :new, :show] do
+    get :activate, on: :collection
+  end
+  
   resource :session, only: [:new, :create, :destroy]
+  
   resources :subs do 
     member do 
       delete :unsubscribe 
       post :subscribe 
     end
   end
+  
   resources :posts, except: :index do 
     resources :comments, only: :new 
     post 'upvote', on: :member
     post 'downvote', on: :member
   end
+  
   resources :comments, only: [:create, :show] do 
     post 'upvote', on: :member
     post 'downvote', on: :member
   end
+
+  root to: redirect("/subs")
 end
