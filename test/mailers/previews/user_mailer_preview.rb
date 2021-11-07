492412a489c069282a34cd5bd199999e06d40f53
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-07 22:08:12 +0200

    Add activation_email method

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# Preview activation_emails at http://localhost:3000/rails/mailers/user_mailer/activation_email?email=user@example.com
 class UserMailerPreview < ActionMailer::Preview
-
+    def activation_email
+        user = User.find_by(email: params[:email]) 
+        UserMailer.activation_email(user)
+    end
 end
+
